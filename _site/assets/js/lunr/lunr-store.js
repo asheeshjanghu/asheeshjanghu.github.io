7758359bca29efbab9192c2ac4f7a80c8829b773
Junghwa Yang <rosie.junghwa@gmail.com>
2019-10-03 14:00:36 +1000

    no message

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -3,4 +3,9 @@ var store = [{
         "excerpt":" ","categories": ["ml"],
         "tags": ["datascience","machinelearning","pandas"],
         "url": "http://localhost:4000/ml/pandas/",
+        "teaser":null},{
+        "title": "Jekyll",
+        "excerpt":"마음에 드는 jekyll theme을 선택하고 터미널에서   gem install jekyll bundler   이렇게 했는데 자꾸 permission 관련 에러가 떠서   sudo gem install jekyll bundler   으로 bundler 설치한후에, theme 다운받은 폴더로 이동해서   bundle exec jekyll serve   실행하면 http://localhost:4000에서 서브된다.   로컬에서 변경사항 확인한 후에 github에 push  ","categories": ["etc"],
+        "tags": ["programming","algorithms","datastructures"],
+        "url": "http://localhost:4000/etc/jekyll/",
         "teaser":null}]
