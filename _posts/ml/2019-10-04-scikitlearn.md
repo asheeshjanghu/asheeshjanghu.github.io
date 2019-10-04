f777ae844848ce73251382c1c0da1aaa3f0d2954
Junghwa Yang <rosie.junghwa@gmail.com>
2019-10-04 15:42:50 +1000

    Upload post 'introml'

--- _posts/ml/2019-10-04-scikitlearn.md
+++ _posts/ml/2019-10-04-scikitlearn.md
@@ -1,6 +1,6 @@
 ---
 title: "Scikit-learn for Machine Learning"
-date: 2019-10-01T12:00:00-04:00
+date: 2019-10-04T12:00:00-04:00
 categories: ml
 tags: [datascience, machinelearning, scikitlearn, sklearn]
 excerpt: "scikit-learn for machine learning"
@@ -9,4 +9,11 @@ author_profile: false
 sidebar:
   nav: "Data Science"
 ---
+
+**Scikit-learn:** Python 머신러닝 라이브러리 중 가장 많이 사용되는 라이브러리. 
+{: .notice--warning}
+
+```python
+import sklearn
+```
 
--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -4,11 +4,6 @@ var store = [{
         "tags": ["datascience","machinelearning","numpy"],
         "url": "http://localhost:4000/ml/numpy/",
         "teaser":null},{
-        "title": "Scikit-learn for Machine Learning",
-        "excerpt":" ","categories": ["ml"],
-        "tags": ["datascience","machinelearning","scikitlearn","sklearn"],
-        "url": "http://localhost:4000/ml/scikitlearn/",
-        "teaser":null},{
         "title": "Hosting on GitHub pages with Jekyll",
         "excerpt":"마음에 드는 jekyll theme을 선택하고 터미널에서   gem install jekyll bundler   이렇게 했는데 자꾸 permission 관련 에러가 떠서   sudo gem install jekyll bundler   으로 bundler 설치한후에, theme 다운받은 폴더로 이동해서   bundle exec jekyll serve   실행하면 http://localhost:4000에서 서브된다.   로컬에서 변경사항 확인한 후에 github에 push  ","categories": ["etc"],
         "tags": ["github","githubpage","jekyll"],
